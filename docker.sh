2f26e3827a31d85bb1b76b69f8deda9ea138bf6d
rahul <mailrahulsre@gmail.com>
2021-09-08 06:44:16 +0000

    docker.sh add

---
+++ docker.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+LOGFILE=/tmp/docker-install.log
+
+touch $LOGFILE
+
+check ()                                #######  Function Defination
+{
+flag=$1
+var=$2                                  ####### Var section
+
+if [ $flag -ne 0 ]; then                        ####### if section
+        echo "Failed at step $var.." >> $LOGFILE
+        exit 1
+else
+        echo "===============Congratulations: Step $var is success======================="
+fi
+
+}
+
+sudo yum install -y yum-utils 
+
+check $? pre-req-install
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+check $? add-docker-repo
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+check $? docker-install
+
+sudo systemctl start docker
+
+check $? start-docker
+
+sudo docker run hello-world
+
+check $? smoke-test
+
+sudo chkconfig docker on
+
+useradd ansadmin
+
